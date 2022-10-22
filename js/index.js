ec8db485b9bf3ef140b312a9eef08d598c768c12
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-10-21 21:19:07 -0300

    aula 2: calculando imc do paciente um

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@
 var titulo = document.querySelector("#titulo-principal");
 
 titulo.textContent = "Aparecida Nutrição";
+
+var tabelaPacientes = document.querySelector("#tabela-pacientes");
+var pacienteUm = tabelaPacientes.querySelector("#paciente-um");
+var massa = pacienteUm.querySelector(".info-peso").textContent;
+var altura = pacienteUm.querySelector(".info-altura").textContent;
+var tdIMC = pacienteUm.querySelector(".info-imc");
+var imc = massa / (altura * altura);
+
+tdIMC.textContent = imc;
+
+console.log(imc);
