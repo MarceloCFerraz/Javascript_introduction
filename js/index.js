c14932e1a6bd199244f7688e4366618866fc920c
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-10-21 22:14:51 -0300

    aula 4: refatorando validações e modicifando background-color quando possuir algum dado incorreto

--- js/index.js
+++ js/index.js
@@ -5,26 +5,24 @@ var titulo = document.querySelector("#titulo-principal");
 titulo.textContent = "Aparecida Nutrição";
 
 var pacientes = document.querySelectorAll(".paciente");
-var imc;
-var erro;
+var imc = 0.00;
+var erro = "";
 
-for(var i = 0, length1 = pacientes.length; i < length1; i++)
+for(var i = 0; i < pacientes.length; i++)
 {
 	var peso = pacientes[i].querySelector(".info-peso").textContent;
 	var altura = pacientes[i].querySelector(".info-altura").textContent;
 	var tdIMC = pacientes[i].querySelector(".info-imc");
 
-	if (peso <= 0 || peso >= 300)
+	if (peso <= 0 || peso >= 300 || altura <= 0.4 || altura >= 4.0)
 	{
-		erro = "Peso inválido";
+		erro = "Dados Inválidos";
 		
-		if (altura <= 0.4 || altura >= 4)
-		{	
-			erro += " | Altura inválida";
-		}
-
 		tdIMC.textContent = erro;
-	} else
+		/pacientes[i].style.backgroundColor = "lightcoral";/
+		pacientes[i].classList.add("paciente-invalido");
+	}
+	else
 	{
 		imc = peso / (altura * altura);
 		imc.toFixed(2)
