19cac8ca767a21e44a7ba27a22b9d8382f97d05b
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-10-22 23:02:01 -0300

    aula 6: script para adicionar novo paciente à tabela. código refatorado e modularizado

--- js/index.js
+++ js/index.js
@@ -1,70 +1,115 @@
-
-
 // # para id 
 // . para classe
-var titulo = document.querySelector("#titulo-principal");
-
-titulo.textContent = "Aparecida Nutrição";
-
 var pacientes = document.querySelectorAll(".paciente");
-var imc = 0.00;
-var erro = "";
 
-for(var i = 0; i < pacientes.length; i++)
-{
-	var peso = pacientes[i].querySelector(".info-peso").textContent;
-	var altura = pacientes[i].querySelector(".info-altura").textContent;
-	var tdIMC = pacientes[i].querySelector(".info-imc");
-
-	if (peso <= 0 || peso >= 300 || altura <= 0.4 || altura >= 4.0)
-	{
-		erro = "Dados Inválidos";
-		
-		tdIMC.textContent = erro;
-		/pacientes[i].style.backgroundColor = "lightcoral";/
-		pacientes[i].classList.add("paciente-invalido");
-	}
-	else
-	{
-		imc = peso / (altura * altura);
-		imc.toFixed(2)
-		tdIMC.textContent = imc.toFixed(2);	
-	}
-}
+adicionarIMCPacientes(pacientes);
 
 var btnAdicionarPaciente = document.querySelector("#adicionar-paciente");
 
 btnAdicionarPaciente.addEventListener("click", AdicionarNovoPaciente);
 
-function AdicionarNovoPaciente(event) {
+function adicionarIMCPacientes(pacientes)
+{
+	var imc = 0.00;
+	var erro = "";
+
+	for(var i = 0; i < pacientes.length; i++)
+	{
+		var peso = pacientes[i].querySelector(".info-peso").textContent;
+		var altura = pacientes[i].querySelector(".info-altura").textContent;
+		var tdIMC = pacientes[i].querySelector(".info-imc");
+
+		if (!pesoValido(peso) || !alturaValida(altura))
+		{
+			erro = "Dados Inválidos";
+			
+			tdIMC.textContent = erro;
+			/pacientes[i].style.backgroundColor = "lightcoral";/
+			pacientes[i].classList.add("paciente-invalido");
+		}
+		else
+		{
+			imc = calcularIMC(peso, altura);
+			tdIMC.textContent = imc;	
+		}
+	}
+}
+
+function pesoValido(peso)
+{
+	return peso > 0 && peso < 300;
+}
+
+function alturaValida(altura)
+{
+	return altura > 0.4 && altura < 4.0;
+}
+
+function calcularIMC(peso, altura)
+{
+	var imc = peso / (altura * altura);
+
+	return imc.toFixed(2);
+}
+
+function AdicionarNovoPaciente(event) 
+{
 	event.preventDefault();
 	
-	var pacientes = document.querySelector("#tabela-pacientes");
+	var pacientesTable = document.querySelector("#tabela-pacientes");
 	var form = document.querySelector("#form-adiciona");
-	var pacienteTR = document.createElement("tr");
 
+	var paciente = extrairDadosPaciente(form);
+	var novoPacienteTR = criarPacienteTR(paciente);
+
+	pacientesTable.appendChild(novoPacienteTR);
+}
+
+function extrairDadosPaciente(form)
+{
+	var paciente = 
+	{
+		nome: form.nome.value,
+		peso: form.peso.value,
+		altura: form.altura.value,
+		gordura: form.gordura.value,
+		imc: calcularIMC(form.peso.value, form.altura.value)
+	};
+
+	return paciente;
+}
+
+function criarPacienteTR(paciente)
+{
+	/ criando a nova Table Row (TR) e Table Datas (TDs) /
+	var pacienteTR = document.createElement("tr");
 	var nomeTD = document.createElement("td");
 	var pesoTD = document.createElement("td");
 	var alturaTD = document.createElement("td");
 	var gorduraTD = document.createElement("td");
 	var imcTD = document.createElement("td");
 
-	var nome = form.nome.value;
-	nomeTD.textContent = nome;
-	var peso = form.peso.value;
-	pesoTD.textContent = peso;
-	var altura = form.altura.value;
-	alturaTD.textContent = altura;
-	var gordura = form.gordura.value;
-	gorduraTD.textContent = gordura;
-	var imc = peso / (altura * altura);
-	imcTD.textContent = imc.toFixed(2);
+	/ adicionando as respectivas classes à TR e aos TDs /
+	pacienteTR.classList.add("paciente");
+	nomeTD.classList.add("info-nome");
+	pesoTD.classList.add("info-peso");
+	alturaTD.classList.add("info-altura");
+	gorduraTD.classList.add("info-gordura");
+	imcTD.classList.add("info-imc");
 
+	/ adicionando os valores do objeto paciente aos seus respectivos TDs /
+	nomeTD.textContent = paciente.nome;
+	pesoTD.textContent = paciente.peso;
+	alturaTD.textContent = paciente.altura;
+	gorduraTD.textContent = paciente.gordura;
+	imcTD.textContent = paciente.imc;
+
+	/ anexando os TDs à TR /
 	pacienteTR.appendChild(nomeTD);
 	pacienteTR.appendChild(pesoTD);
 	pacienteTR.appendChild(alturaTD);
 	pacienteTR.appendChild(gorduraTD);
 	pacienteTR.appendChild(imcTD);
 
-	pacientes.appendChild(pacienteTR);
+	return pacienteTR;
 }
