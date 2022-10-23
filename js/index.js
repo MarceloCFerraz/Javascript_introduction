3d077c4d8831de7ea971b1d4ac1648ba557c7192
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-10-22 21:58:59 -0300

    aula 5: adicionado form para adicionar novos pacientes e pré-programado função para adicionar à tabela

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 // # para id 
 // . para classe
 var titulo = document.querySelector("#titulo-principal");
@@ -30,4 +32,39 @@ for(var i = 0; i < pacientes.length; i++)
 	}
 }
 
+var btnAdicionarPaciente = document.querySelector("#adicionar-paciente");
 
+btnAdicionarPaciente.addEventListener("click", AdicionarNovoPaciente);
+
+function AdicionarNovoPaciente(event) {
+	event.preventDefault();
+	
+	var pacientes = document.querySelector("#tabela-pacientes");
+	var form = document.querySelector("#form-adiciona");
+	var pacienteTR = document.createElement("tr");
+
+	var nomeTD = document.createElement("td");
+	var pesoTD = document.createElement("td");
+	var alturaTD = document.createElement("td");
+	var gorduraTD = document.createElement("td");
+	var imcTD = document.createElement("td");
+
+	var nome = form.nome.value;
+	nomeTD.textContent = nome;
+	var peso = form.peso.value;
+	pesoTD.textContent = peso;
+	var altura = form.altura.value;
+	alturaTD.textContent = altura;
+	var gordura = form.gordura.value;
+	gorduraTD.textContent = gordura;
+	var imc = peso / (altura * altura);
+	imcTD.textContent = imc.toFixed(2);
+
+	pacienteTR.appendChild(nomeTD);
+	pacienteTR.appendChild(pesoTD);
+	pacienteTR.appendChild(alturaTD);
+	pacienteTR.appendChild(gorduraTD);
+	pacienteTR.appendChild(imcTD);
+
+	pacientes.appendChild(pacienteTR);
+}
