4f32ed2ca537d7de7f0dd01198b7e3ed0c872c57
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-10-24 22:08:38 -0300

    aula 7: implementada função de deletar paciente da tabela. função exclui pacientes já na tabela e pacientes adicionados posteriormente

---
+++ js/IMCController.js
@@ -0,0 +1,51 @@
+// # para id 
+// . para classe
+var pacientes = document.querySelectorAll(".paciente");
+
+adicionarIMCPacientes(pacientes);
+
+var btnAdicionarPaciente = document.querySelector("#adicionar-paciente");
+
+function adicionarIMCPacientes(pacientes)
+{
+	var imc = 0.00;
+	var erro = "";
+
+	for(var i = 0; i < pacientes.length; i++)
+	{
+		var peso = pacientes[i].querySelector(".info-peso").textContent;
+		var altura = pacientes[i].querySelector(".info-altura").textContent;
+		var tdIMC = pacientes[i].querySelector(".info-imc");
+
+		if (!pesoValido(peso) || !alturaValida(altura))
+		{
+			erro = "Dados Inválidos";
+			
+			tdIMC.textContent = erro;
+			/pacientes[i].style.backgroundColor = "lightcoral";/
+			pacientes[i].classList.add("paciente-invalido");
+		}
+		else
+		{
+			imc = calcularIMC(peso, altura);
+			tdIMC.textContent = imc;	
+		}
+	}
+}
+
+function pesoValido(peso)
+{
+	return (peso > 0 && peso < 300);
+}
+
+function alturaValida(altura)
+{
+	return altura > 0.4 && altura < 4.0;
+}
+
+function calcularIMC(peso, altura)
+{
+	var imc = peso / (altura * altura);
+
+	return imc.toFixed(2);
+}
---
+++ js/PacienteController.js
@@ -0,0 +1,152 @@
+btnAdicionarPaciente.addEventListener("click", AdicionarNovoPaciente);
+
+ativarBotaoDelete(document.querySelector("#tabela-pacientes"));
+
+function ativarBotaoDelete (tabela) {
+	tabela.addEventListener("click", function(event)
+	{
+		var alvo = event.target;
+		if (alvo.classList == "btn-deletar")
+		{
+			// o botão de deletar foi clicado
+			alvo.closest(".paciente").remove();
+		}
+	});
+}
+
+function deletarPaciente() {
+	console.log("Estou sendo executado")
+}
+
+function AdicionarNovoPaciente(event) 
+{
+	event.preventDefault();
+		
+	var pacientesTable = document.querySelector("#tabela-pacientes");
+
+	var form = document.querySelector("#form-adiciona");
+
+	var paciente = extrairPacienteDoForm(form);
+	
+	if (paciente == null)
+	{
+		alert("ALGO DADO ESTÁ INCORRETO. Por favor, corrija os campos em vemelho e tente novamente!");
+	}
+	else
+	{
+		var novoPacienteTR = criarTrDePaciente(paciente); 
+
+		pacientesTable.appendChild(novoPacienteTR);
+
+		resetForm(form);
+	}
+}
+
+
+function extrairPacienteDoForm(form)
+{
+	var paciente = 
+	{
+		nome: form.nome.value,
+		peso: form.peso.value,
+		altura: form.altura.value,
+		gordura: form.gordura.value,
+		imc: calcularIMC(form.peso.value, form.altura.value)
+	};
+
+	var retorno = validarPaciente(paciente, form);
+
+	return retorno;
+}
+
+function validarPaciente (paciente) {
+	var retorno = paciente;
+
+	if (!nomeValido(paciente.nome))
+	{
+		retorno = null;
+		highlightErrors("nome", form);
+	}
+
+	if (!pesoValido(paciente.peso) || paciente.peso.length == 0)
+	{
+		retorno = null;
+		highlightErrors("peso", form);
+	}
+
+	if (!alturaValida(paciente.altura) || paciente.altura.length == 0)
+	{
+		retorno = null;
+		highlightErrors("altura", form);
+	}
+
+	if (paciente.gordura.length == 0)
+	{
+		retorno = null;
+		highlightErrors("gordura", form);
+	}
+
+	return retorno;
+}
+
+function nomeValido(nome)
+{
+	return nome.length > 0;
+}
+
+function highlightErrors(campo, form)
+{
+	form.querySelector("#" + campo).style.color = "red";
+}
+
+function criarTrDePaciente(paciente)
+{
+	// criando a nova Table Row (TR) e adicionando classe "paciente" ao html
+	var pacienteTR = document.createElement("tr");
+	pacienteTR.classList.add("paciente");
+
+	// criando Table Datas (TDs) e adicionando suas respectivas classes e dados
+	var nomeTD = criarTdDePaciente(paciente.nome, "info-nome");
+	var pesoTD = criarTdDePaciente(paciente.peso, "info-peso");
+	var alturaTD = criarTdDePaciente(paciente.altura, "info-altura");
+	var gorduraTD = criarTdDePaciente(paciente.gordura, "info-gordura");
+	var imcTD = criarTdDePaciente(paciente.imc, "info-imc");
+
+	// Criando botão de deleção
+	var btnDeletar = document.createElement("button");
+	btnDeletar.classList.add("btn-deletar");
+	btnDeletar.textContent = "X";
+	btnDeletar.onclick = deletarPaciente;
+
+	// Criando TD para o botão delete
+	var btnTD = document.createElement("td");
+	btnTD.classList.add("info-deletar");
+	btnTD.appendChild(btnDeletar);
+
+	// anexando os TDs à TR
+	pacienteTR.appendChild(nomeTD);
+	pacienteTR.appendChild(pesoTD);
+	pacienteTR.appendChild(alturaTD);
+	pacienteTR.appendChild(gorduraTD);
+	pacienteTR.appendChild(imcTD);
+	pacienteTR.appendChild(btnTD);
+
+	return pacienteTR;
+}
+
+function criarTdDePaciente (dado, classe) {
+	var td = document.createElement("td");
+	td.textContent = dado;
+	td.classList.add(classe);
+
+	return td
+}
+
+function resetForm(form)
+{
+	form.reset();
+	form.querySelector("#nome").style.color = "black";
+	form.querySelector("#altura").style.color = "black";
+	form.querySelector("#peso").style.color = "black";
+	form.querySelector("#gordura").style.color = "black";
+}
